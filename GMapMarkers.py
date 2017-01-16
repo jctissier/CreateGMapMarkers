62be37123545f3a8031759ee5adf083d1c4ad1d6
Jean-Claude Tissier <jeanclaude.tca@gmail.com>
2017-01-15 19:56:51 -0800

    Update GMapMarkers.py

--- GMapMarkers.py
+++ GMapMarkers.py
@@ -274,24 +274,24 @@ class CreateGoogleMapMarkers(object):
         f.write('\n')
 
 #Example -> creates 'test.html'
-if __name__ == '__main__':
-    black_lat = ['49.284040', '49.283190']
-    black_lon = ['-123.115950', '-123.115680']
-    black_titles = ['black 1', 'black 2']
-    red_lat = ['49.283460', '49.284490', '49.284740']
-    red_lon = ['-123.116560', '-123.115000', '-123.115170']
-    red_titles = ['red 1', 'red 2', 'red 3']
-    yellow_lat = ['49.284740', '49.284790', '49.284580', '49.284580']
-    yellow_lon = ['-123.115170', '-123.115240', '-123.114610', '-123.117080']
-    yellow_titles = ['yellow 1', 'yellow 2', 'yellow 3', 'yellow 4']
-    red_lat2 = ['49.284790', '49.285050']
-    red_lon2 = ['-123.116780', '-123.116350']
-    red_titles2 = ['red 4', 'red 5']
+# if __name__ == '__main__':
+#     black_lat = ['49.284040', '49.283190']
+#     black_lon = ['-123.115950', '-123.115680']
+#     black_titles = ['black 1', 'black 2']
+#     red_lat = ['49.283460', '49.284490', '49.284740']
+#     red_lon = ['-123.116560', '-123.115000', '-123.115170']
+#     red_titles = ['red 1', 'red 2', 'red 3']
+#     yellow_lat = ['49.284740', '49.284790', '49.284580', '49.284580']
+#     yellow_lon = ['-123.115170', '-123.115240', '-123.114610', '-123.117080']
+#     yellow_titles = ['yellow 1', 'yellow 2', 'yellow 3', 'yellow 4']
+#     red_lat2 = ['49.284790', '49.285050']
+#     red_lon2 = ['-123.116780', '-123.116350']
+#     red_titles2 = ['red 4', 'red 5']
 
-    test = CreateGoogleMapMarkers(49.283140, -123.115950, 16, 'default', '<b>Current Location!</b>')
-    test.set_api_key('API_KEY')
-    test.create_markers(red_lat, red_lon, red_titles, 'red')
-    test.create_markers(black_lat, black_lon, 'None', 'black')
-    test.create_markers(yellow_lat, yellow_lon, yellow_titles, 'yellow')
-    test.create_markers(red_lat2, red_lon2, red_titles2, 'red')
-    test.map_markers('test')
+#     test = CreateGoogleMapMarkers(49.283140, -123.115950, 16, 'default', '<b>Current Location!</b>')
+#     test.set_api_key('API_KEY')
+#     test.create_markers(red_lat, red_lon, red_titles, 'red')
+#     test.create_markers(black_lat, black_lon, 'None', 'black')
+#     test.create_markers(yellow_lat, yellow_lon, yellow_titles, 'yellow')
+#     test.create_markers(red_lat2, red_lon2, red_titles2, 'red')
+#     test.map_markers('test')
