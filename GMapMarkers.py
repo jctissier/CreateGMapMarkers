36f4e5fa79b17efe9e7b49dffbdaf3c37898bc6f
Jean-Claude Tissier <jeanclaude.tca@gmail.com>
2017-01-15 18:39:52 -0800

    Update GMapMarkers.py

--- GMapMarkers.py
+++ GMapMarkers.py
@@ -49,3 +49,37 @@ class CreateGoogleMapMarkers(object):
     def set_api_key(key):
         global API_KEY
         API_KEY = key
+        
+    def map_markers(self, file_name):
+        # Top of HTML file -> CONSTANT
+        f = open(file_name + '.html', 'w')
+        f.write('<!DOCTYPE html>\n')
+        f.write('<html>\n')
+        f.write('<head>\n')
+        f.write('\t<meta http-equiv="content-type" content="text/html; charset=UTF-8" />\n')
+        f.write('\t<title>Google Maps Markers</title>\n')
+        f.write('\t<script src="http://maps.google.com/maps/api/js?key=' + API_KEY +
+                '&sensor=true_or_false&libraries=visualization" type="text/javascript"></script>\n')
+        f.write('</head>\n')
+        f.write('<body style="margin:0px; padding:0px;" onload="initialize()">\n')
+        f.write('\t<div id="map" style="width: 1500px; height: 1000px;"></div>\n')
+        f.write('\t<script type="text/javascript">\n')
+        f.write('\t\tfunction initialize() {\n')
+        # Write current location lat & lons
+        self.add_center_location(f)
+        # create locations with different colored markers
+        self.set_white_markers(f)
+        self.set_black_markers(f)
+        self.set_red_markers(f)
+        self.set_purple_markers(f)
+        self.set_green_markers(f)
+        self.set_orange_markers(f)
+        self.set_blue_markers(f)
+        self.set_yellow_markers(f)
+        # Bottom of HTML file -> CONSTANT
+        f.write('\t\t\tsetMap(locationMarker);\n')
+        f.write('\t\t}\n')
+        f.write('\t</script>\n')
+        f.write('</body>\n')
+        f.write('</html>\n')
+        f.close()
